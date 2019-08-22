914c83ff95113ddea94bff3519a0eb39847b2e7e
Olivia Cobble <ocobble@hotmail.com>
2019-08-22 10:06:18 -0500

    Kind of works but needs testing

--- Dewey/src/BookSort.java
+++ Dewey/src/BookSort.java
@@ -8,10 +8,34 @@ public class BookSort {
 		String[] callNumbers = keyboard.nextLine().split(",");
 		
 		// Sort the inputs
+		for (int i = 1; i < callNumbers.length; ++i) {
+			String key = callNumbers[i]; 
+            int j = i - 1; 
+  
+            /* Move elements of arr[0..i-1], that are 
+               greater than key, to one position ahead 
+               of their current position */
+            while (j >= 0 && key.compareTo(callNumbers[j]) < 0) { 
+                callNumbers[j + 1] = callNumbers[j]; 
+                j = j - 1; 
+            } 
+            callNumbers[j + 1] = key; 
+		}
 		
 		// Print sorted list
 		for (int i = 0; i < callNumbers.length; ++i) {
 			System.out.println(callNumbers[i]);
 		}
+	}
+	
+	public static int bookComparator(String book1, String book2) {
+		
+		// Tokenize pieces of call numbers
+		
+		String[] callNumber1 = book1.split(" ");
+		String[] callNumber2 = book2.split(" ");
+		
+		
+		return 0;
 	}
 }
